7868b16b38b85c80cccd695d274bc11178d92bce
Satyam dhasmana <113670607+satyamDhasmana28@users.noreply.github.com>
2023-06-12 20:24:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const element = document.getElementById("colorSelect");
+const button = document.querySelector("form input");
+let deleteFromSelectElement = (val)=>{
+    if(!val){
+        return ;
+    }
+    for(let i=0 ; i<element.children.length ; i++){
+        if(val===element.children[i].value){
+            element.children[i].remove();
+            return;
+        }
+    }
+}
+button.addEventListener('click',()=>{
+  let value = element.value;
+  deleteFromSelectElement(value);
+});
